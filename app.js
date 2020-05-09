f599d48d3c8bde7dc284096346c5179d16483e98
gabrieldapaz <mgpp@cin.ufpe.br>
2020-05-09 10:18:13 -0300

    installing packages and seting inital routes of the app

---
+++ app.js
@@ -0,0 +1,20 @@
+var express = require("express");
+var app = express();
+
+app.set("view engine", "ejs")
+
+app.get("/", function(req, res){
+    res.render("landing");
+});
+
+app.get("/campgrounds", function(req, res){
+    res.render("landing");
+});
+
+app.get("*", function(req, res){
+    res.send("PAGE NOT FOUND :(");
+});
+
+app.listen(3000, function(){
+    console.log("Server is running...");
+});
