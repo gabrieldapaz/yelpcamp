d35166337b19f6be9261975ee86251196b4f025c
gabrieldapaz <mgpp@cin.ufpe.br>
2020-05-13 14:01:02 -0300

    styling the show page, adding the public folder

--- seeds.js
+++ seeds.js
@@ -6,17 +6,17 @@ var data = [
     {
         name: "Cloud's Rest",
         image: "https://media-cdn.tripadvisor.com/media/photo-o/13/b8/8a/28/olakira-camp-asilia-africa.jpg",
-        description: "blah blah blah blah"
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis ac ipsum porttitor, dignissim libero sit amet, tempus quam. Integer velit tellus, porttitor in bibendum vitae, porta sit amet velit. In et aliquet orci, quis pulvinar libero. Mauris lacinia eget sem sit amet egestas. Mauris at ante non nunc consectetur finibus. Maecenas aliquam justo non lacus venenatis, eu euismod felis pellentesque."
     },
     {
         name: "Desert Mesa",
         image: "https://q-xx.bstatic.com/xdata/images/hotel/840x460/196377895.jpg?k=50abd15a8d47232fc79dddebe6b56249e2abd55844165a5e0d47cb4b1d3ab925&o=",
-        description: "blah blah blah blah"
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis ac ipsum porttitor, dignissim libero sit amet, tempus quam. Integer velit tellus, porttitor in bibendum vitae, porta sit amet velit. In et aliquet orci, quis pulvinar libero. Mauris lacinia eget sem sit amet egestas. Mauris at ante non nunc consectetur finibus. Maecenas aliquam justo non lacus venenatis, eu euismod felis pellentesque."
     },
     {
         name: "Canyon Floor",
         image: "https://i.zst.com.br/images/8-melhores-barracas-de-camping-em-2018-voce-compra-aqui-photo240843911-44-1b-17.jpg",
-        description: "blah blah blah blah"
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis ac ipsum porttitor, dignissim libero sit amet, tempus quam. Integer velit tellus, porttitor in bibendum vitae, porta sit amet velit. In et aliquet orci, quis pulvinar libero. Mauris lacinia eget sem sit amet egestas. Mauris at ante non nunc consectetur finibus. Maecenas aliquam justo non lacus venenatis, eu euismod felis pellentesque."
     }
 ];
 
