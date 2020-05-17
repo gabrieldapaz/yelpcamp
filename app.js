b7c825c4a090e18e277cfdce07f48c473ea11e0b
gabrieldapaz <mgpp@cin.ufpe.br>
2020-05-17 14:10:33 -0300

    add local port 3000

--- app.js
+++ app.js
@@ -49,6 +49,6 @@ app.use("/",indexRoutes);
 app.use("/campgrounds",campgroundRoutes);
 app.use("/campgrounds/:id/comments",commentRoutes);
 
-app.listen(process.env.PORT, function(){
+app.listen(process.env.PORT || 3000, function(){
     console.log("Server is running...");
 });
