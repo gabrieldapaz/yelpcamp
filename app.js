6597f7fced7f320e393dc79bb39969e067d941d9
gabrieldapaz <mgpp@cin.ufpe.br>
2020-05-17 01:08:35 -0300

    add mongo atlas

--- app.js
+++ app.js
@@ -17,7 +17,8 @@ var commentRoutes = require("./routes/comments"),
     indexRoutes = require("./routes/index");
 
 //seedDB();
-mongoose.connect("mongodb://localhost/yelp_camp", {useUnifiedTopology:true, useNewUrlParser:true});
+//mongoose.connect("mongodb://localhost/yelp_camp", {useUnifiedTopology:true, useNewUrlParser:true});
+mongoose.connect("mongodb+srv://gdapaz:yelpcamp159@yelpcamp-6at8f.mongodb.net/test?retryWrites=true&w=majority");
 app.use(bodyParser.urlencoded({extended:true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
